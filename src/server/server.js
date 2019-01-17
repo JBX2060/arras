2a0a973c3aab6266037d749153e94c25b6e50612
Glitch (artemix) <none>
2019-01-17 00:59:05 +0000

    📰✨ Checkpoint ./src/server/server.js:2752431/65

--- src/server/server.js
+++ src/server/server.js
@@ -2013,7 +2013,7 @@ class Entity {
         this.acceleration = c.runSpeed * this.ACCELERATION / speedReduce;
         if (this.settings.reloadToAcceleration) this.acceleration *= this.skill.acl;
 
-        this.topSpeed = c.runSpeed * this.SPEED * this.skill.mob / speedReduce;
+        this.topSpeed = c.runSpeed * (this.SPEED * 1.5) * this.skill.mob / speedReduce;
         if (this.settings.reloadToAcceleration) this.topSpeed /= Math.sqrt(this.skill.acl);
         
         this.health.set(
@@ -2340,9 +2340,9 @@ class Entity {
     friction() {        
         var motion = this.velocity.length, excess = motion - this.maxSpeed;
         if (excess > 0 && this.damp) {
-          var k = this.damp * timestep * 0.97, drag = excess / (k + 1), finalVelocity = this.maxSpeed + drag;
-          this.velocity.x = ((finalVelocity * this.velocity.x / motion));
-          this.velocity.y = ((finalVelocity * this.velocity.y / motion));
+          var k = this.damp * roomSpeed * 0.97, drag = excess / (k + 1), finalVelocity = this.maxSpeed + drag;
+          this.velocity.x = ((finalVelocity * this.velocity.x / motion)) * 0.97;
+          this.velocity.y = ((finalVelocity * this.velocity.y / motion)) * 0.97;
         }
     }
 
@@ -4595,9 +4595,9 @@ var gameloop = (() => {
     // Return the loop function
     return () => {
         var curTime = now();
-        timestep = 0.0025 * (curTime - lastTime);
+        timestep = 0.0075 * (curTime - lastTime);
         if (timestep <= 0 || timestep > 1.0) {
-            timestep = 0.0025;
+            timestep = 0.0075;
         }
         logs.loops.tally();
         logs.master.set();
