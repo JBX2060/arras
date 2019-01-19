a50805a76262e298738d57e284ecb28bedd54f1a
Glitch (artemix) <none>
2019-01-19 22:41:03 +0000

    🚆🛎 Checkpoint ./src/server/server.js:2752431/4

--- bin/server/server.js
+++ bin/server/server.js
@@ -1985,7 +1985,7 @@ class Entity {
         this.acceleration = c.runSpeed * this.ACCELERATION / speedReduce;
         if (this.settings.reloadToAcceleration) this.acceleration *= this.skill.acl;
 
-        this.topSpeed = c.runSpeed * (this.SPEED * 4.625) * this.skill.mob / speedReduce / 2.795;
+        this.topSpeed = c.runSpeed * (this.SPEED * 4.655) * this.skill.mob / speedReduce / 2.795;
         if (this.settings.reloadToAcceleration) this.topSpeed /= Math.sqrt(this.skill.acl);
 
         this.health.set(((this.settings.healthWithLevel ? 2 * this.skill.level : 0) + this.HEALTH) * this.skill.hlt);
